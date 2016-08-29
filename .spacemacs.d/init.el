875fce0e52bc5a72ceef3a36c39309a920dbb747
Kenji MIYAZAKI <kenjimyzk@gmail.com>
2016-08-29 10:27:48 +0900

    keybinding (C-h)

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -249,7 +249,10 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  )
+  ;; keybinding
+  (global-set-key (kbd "C-h") 'delete-backward-char)
+  (global-set-key (kbd "<F1>") help-map);
+)
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
