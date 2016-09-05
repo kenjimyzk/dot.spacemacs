22b40b1a36eb9faf76a84a1c7bfa75434fc770f4
Kenji Miyazaki <kenjimyzk@gmail.com>
2016-09-05 11:58:09 +0900

    packages

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -40,7 +40,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(mozc)
+   dotspacemacs-additional-packages '(mozc migemo avy-migemo avy-zap) 
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
